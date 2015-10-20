9d04ee532255413ff1389ec65fe70768e7b3b403
XuZhengrong <zrxu2006@hotmail.com>
2015-10-20 17:53:59 +0800

    追加Table

---
+++ WebApplication1/DBDocumentEditor.Domain/Entities/DBTable.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DBDocumentEditor.Domain.Entities
+{
+    public class DBTable
+    {
+        public string Name { get; set; }
+
+        public int ObjectID { get; set; }
+    }
+}
