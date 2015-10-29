ec3d9e4be24808181da210f9a6931f09f2482f6a
XuZhengrong <zrxu2006@hotmail.com>
2015-10-29 15:57:08 +0800

    task demo

---
+++ WebApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TaskDemo.Start(args);
+            ////使用反射读取Attribute
+            //System.Reflection.MemberInfo info = typeof(Student); //通过反射得到Student类的信息
+            //Hobby hobbyAttr = (Hobby)Attribute.GetCustomAttribute(info, typeof(Hobby));
+            //var attr = TypeDescriptor.GetAttributes(typeof(Student))[0] as Hobby;
+            
+            //if (hobbyAttr != null)
+            //{
+            //    Console.WriteLine("类名：{0}", info.Name);
+            //    Console.WriteLine("兴趣类型：{0}", hobbyAttr.Type);
+            //    Console.WriteLine("兴趣指数：{0}", hobbyAttr.Level);
+            //}
+
+            //Console.ReadKey();
+        }
+    }
+
+    [Hobby("Sports", Level = 5)]
+    class Student
+    {
+        [Hobby("Football")]
+        public string profession;
+        public string Profession
+        {
+            get { return profession; }
+            set { profession = value; }
+        }
+    }
+    //建议取名：HobbyAttribute
+    class Hobby : Attribute // 必须以System.Attribute 类为基类
+    {
+        // 参数值为null的string 危险，所以必需在构造函数中赋值
+        public Hobby(string _type) // 定位参数
+        {
+            this.type = _type;
+        }
+        //兴趣类型
+        private string type;
+        public string Type
+        {
+            get { return type; }
+            set { type = value; }
+        }
+        //兴趣指数
+        private int level;
+        public int Level
+        {
+            get { return level; }
+            set { level = value; }
+        }
+    }
+
+}
---
+++ WebApplication1/ConsoleApplication1/TaskDemo.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class TaskDemo
+    {
+        static SemaphoreSlim _sem = new SemaphoreSlim(3); 
+        public static void Start(string[] args)
+        {
+            //var dayName = Task.Run<string>(() => { return GetDayOfThisWeek(); });
+            
+            var dayName = Task.Run<string>(() => {
+                Console.WriteLine("dayName：Thread Id {0}", Thread.CurrentThread.ManagedThreadId);
+                return GetDayOfThisWeek();
+            });
+
+            dayName.GetAwaiter().OnCompleted(() =>
+            {
+                Console.WriteLine("今天是：{0}", dayName.Result);
+            });
+
+            //var dayName1 = Task.Run<string>(() =>
+            //{
+            //    Console.WriteLine("dayName1：Thread Id {0}", Thread.CurrentThread.ManagedThreadId);
+            //    return GetDayOfThisWeekAsync();
+            //});
+            //Console.WriteLine("今天是：{0}", dayName.Result);
+            Console.WriteLine("1今天是：{0}", DateTime.Now);
+            //for (int i = 1; i <= 5; i++)
+            //{
+            //    //Task.Factory.StartNew(() =>
+            //    //{                    
+            //    //    Enter1(i);
+            //    //});
+
+            //    Task.Run(() =>
+            //    {
+            //        Enter1(i);
+            //    });
+
+            //    new Task(() =>
+            //    {
+            //        Enter1(i);
+            //    }).Start();
+            //    Enter(i).Start();
+            //}
+            Console.ReadLine();
+        }
+
+        private static string GetDayOfThisWeek()
+        {
+            
+            Thread.Sleep(5000);
+            Console.WriteLine("GetDayOfThisWeek：Thread Id {0}", Thread.CurrentThread.ManagedThreadId);
+            return DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss");
+        }
+
+        static void Enter1(object id)
+        {
+            Console.WriteLine(id + " 开始排队...");
+            _sem.Wait();
+            Console.WriteLine(id + " 开始执行！");
+            Thread.Sleep(1000 * (int)id);
+            Console.WriteLine(id + " 执行完毕，离开！");
+            _sem.Release();
+        }
+
+        static Task Enter(int i)
+        {
+            return new Task(() =>
+            {
+                Console.WriteLine(i + " 开始排队...");
+                _sem.Wait();
+                Console.WriteLine(i + " 开始执行！");
+                Thread.Sleep(5000);
+                Console.WriteLine(i + " 执行完毕，离开！");
+                _sem.Release();
+            });
+        }
+
+        static Task<string> GetDayOfThisWeekAsync()
+        {
+            Console.WriteLine("GetDayOfThisWeekAsync：Thread Id {0}", Thread.CurrentThread.ManagedThreadId);
+            //return new Task<string>(GetDayOfThisWeek);
+            Console.WriteLine("GetDayOfThisWeekAsync：Thread Id {0}", Thread.CurrentThread.ManagedThreadId);
+            var tsk = Task.Run<string>(() =>
+            {
+                Console.WriteLine("tsk：Thread Id {0}", Thread.CurrentThread.ManagedThreadId);
+                return GetDayOfThisWeek();
+            });
+
+            //await tsk;
+            Console.WriteLine("今天是：{0}", tsk.Result);
+
+            return tsk;
+        }
+    }
+}
